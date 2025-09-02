458a81c15677ef872355bc323d9a9467ab215fad
henriquercz <henriquechagas06@gmail.com>
2025-09-01 21:39:01 -0300

    versao 2.6.5

--- components/SplashScreen.tsx
+++ components/SplashScreen.tsx
@@ -145,7 +145,7 @@ export default function SplashScreen({ onFinish }: SplashScreenProps) {
       {/* Versão */}
       <View style={styles.versionContainer}>
         <Text style={[styles.versionText, { color: colors.textSecondary }]}>
-          v2.6.4
+          v2.6.5
         </Text>
       </View>
     </Animated.View>
